6bd5076c379e6fdb3b20163b207f440c819171f4
Max Felis <max.felis11@gmail.com>
2025-06-07 11:16:59 +0200

    chore: comment out PHPStan and notification steps in CI/CD workflow; update PasswordUpdateData to include MapName attributes

--- src/Domain/Settings/Data/PasswordUpdateData.php
+++ src/Domain/Settings/Data/PasswordUpdateData.php
@@ -6,17 +6,20 @@ use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Attributes\Validation\Required;
 use Spatie\LaravelData\Attributes\Validation\Confirmed;
 use Spatie\LaravelData\Attributes\Validation\Min;
+use Spatie\LaravelData\Attributes\MapName;
 
 class PasswordUpdateData extends Data
 {
     public function __construct(
         #[Required]
+        #[MapName('current_password')]
         public string $currentPassword,
         
         #[Required, Min(8), Confirmed]
         public string $password,
         
         #[Required]
+        #[MapName('password_confirmation')]
         public string $passwordConfirmation,
     ) {}
 } 
